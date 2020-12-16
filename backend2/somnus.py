52674fdc458ec16da37f0645fe3a30692a366b1d
Herzogin <s0550233@htw-berlin.de>
2020-12-16 20:31:10 +0100

    Refactoring sleep detection: removed unused code, provided comments

--- backend2/somnus.py
+++ backend2/somnus.py
@@ -24,18 +24,21 @@ allowed_rest_break = 60
 min_t = 25.0  # temperature_threshold
 
 
-def split_days_geneactiv_csv():
-    """Splits the GeneActiv accelerometer data into 24 hour chunks, defined from noon to noon."""
+def split_csv_acc_data_in_days():
+    """
+    Splits the accelerometer data into 24 hour chunks, defined from noon to noon.
+    Saves each 24 hour chunk as HDF file in subdirectory: "raw_acc_data_24hr".
+    """
     os.mkdir(sub_dst + "/raw_days")
     # load data and fix time_stamps
     data = pd.read_csv(
         input_file,
-        # Column(s) to use as the row labels of the DataFrame, either given as string name or column index.
-        index_col=0,
-        skiprows=100,
+
+        index_col=0,  # Columns to use as the row labels of the DataFrame, either given as string name or column index.
+        skiprows=100,  # How many rows should be ignored from begin of CSV file?
         header=None,
-        names=["Time", "X", "Y", "Z", "LUX", "Button", "T", "A", "B", "C", "D", "E"],
-        usecols=["Time", "X", "Y", "Z", "LUX", "T"],
+        names=["Time", "X", "Y", "Z", "LUX", "Button", "T", "A", "B", "C", "D", "E"],  # names of the columns
+        usecols=["Time", "X", "Y", "Z", "LUX", "T"],  # which columns will be used?
         dtype={
             "Time": object,
             "X": np.float64,
@@ -51,13 +54,12 @@ def split_days_geneactiv_csv():
             "E": np.float64
 
         },
-        low_memory=False,
+        low_memory=False,  # how much RAM has the system?
     )
     data.index = pd.to_datetime(data.index, format="%Y-%m-%d %H:%M:%S:%f").values
     data = data.loc[
            data.index[0]
-           + pd.Timedelta(start_buffer): data.index[-1]
-                                             - pd.Timedelta(stop_buffer)
+           + pd.Timedelta(start_buffer): data.index[-1] - pd.Timedelta(stop_buffer)
            ]
     # cut to defined start and end times if specified
     start_time = ""
@@ -95,7 +97,7 @@ def split_days_geneactiv_csv():
             results_directory = "/raw_days/{}_day_{}.h5".format(
                 src_name, str(count).zfill(2)
             )
-            df.to_hdf(sub_dst + results_directory, key="raw_geneactiv_data_24hr", mode="w")
+            df.to_hdf(sub_dst + results_directory, key="raw_acc_data_24hr", mode="w")
     return
 
 
@@ -233,7 +235,10 @@ def activity_index(signal_df, channels=["X", "Y", "Z"]):
 
 
 def extract_activity_index():
-    """Calculates the activity index feature on each 24 hour day."""
+    """
+    Calculates the activity index feature from accelerometer data on each 24 hour day.
+    Saves one HDF file per day with activity index per timestamp in subdirectory "activity_index_data_24hr".
+    """
     os.mkdir(sub_dst + "/activity_index_days")
     count = 0
     # get days
@@ -247,7 +252,7 @@ def extract_activity_index():
     for day in days:
         count += 1
         # load data
-        df = pd.read_hdf(day)
+        df = pd.read_hdf(day)  # reads HDF file of 24 hour chunk of accelerometer data.
         activity = []
         header = ["Time", "activity_index"]
         idx = 0
@@ -290,154 +295,12 @@ def extract_activity_index():
         )
 
 
-def roll_med(df, num_samples):
-    """Calculate the rolling median of a pandas dataframe.
-    :param df: pandas dataframe
-    :param num_samples: number of samples to include in the window
-    :return: pandas dataframe containing the rolling median values."""
-
-    # initialize indexer and rolling median list
-    idx = 0
-    med = []
-    while idx < len(df) - num_samples:
-        med.append(
-            [df.index[idx], df.iloc[idx: idx + num_samples].median()]
-        )  # get start index, std value
-        idx += 1
-    # format data frame
-    df = pd.DataFrame(med, columns=["Time", "Data"])
-    df.set_index(df.Time, inplace=True)
-    df.drop(inplace=True, columns="Time")
-    return df
-
-
-def major_rest_period():
-    """Determines the major rest period"""
-    print("in major rest methode")
-    os.mkdir(sub_dst + "/major_rest_period")
-    count = 0
-    mrps = []
-    header = ["day", "major_rest_period", "available_hours"]
-    # get days
-    days = sorted(
-        [
-            sub_dst + "/raw_days/" + i
-            for i in os.listdir(sub_dst + "/raw_days/")
-            if ".DS_Store" not in i
-        ]
-    )
-    for day in days:
-        df = pd.read_hdf(day)
-        df = df[["X", "Y", "Z", "T"]]
-        available_hours = len(df) / 3600.0
-        # available_hours = (len(df) / float(fs)) / 3600.0
-        count += 1
-
-        # process data
-        df = df.rolling(int(5 * fs)).median()  # run rolling median 5 second
-        df["angle"] = np.arctan(
-            df["Z"] / ((df["X"] ** 2 + df["Y"] ** 2) ** 0.5)
-        ) * (
-            180.0 / np.pi
-        )  # get angle
-
-        df = (
-            df[["angle", "T"]].resample("5s").mean().fillna(0)
-        )  # get 5 second average
-
-        # save intermediate data for plotting
-        df["angle"].to_hdf(
-            sub_dst
-            + "/major_rest_period/5_second_average_arm_angle_day_{}.h5".format(
-                str(count).zfill(2)
-            ),
-            key="arm_angle_data_24hr",
-            mode="w",
-        )
-
-        df["angle"] = np.abs(
-            df["angle"] - df["angle"].shift(1)
-        )  # get absolute difference
-        df_angle = roll_med(df["angle"], 60)  # run rolling median 5 minute
-        df_temp = roll_med(df["T"], 60)  # run rolling median 5 minute
-
-        # calculate and apply threshold
-        thresh = np.min(
-            [
-                np.max(
-                    [
-                        np.percentile(df_angle.Data.dropna().values, 10) * 15.0,
-                        minimum_rest_threshold,
-                    ]
-                ),
-                maximum_rest_threshold,
-            ]
-        )
-        df_angle.Data[df_angle.Data < thresh] = 0  # apply threshold
-        df_angle.Data[df_angle.Data >= thresh] = 1  # apply threshold
-
-        # drop rest periods where temperature is below the temp threshold
-        df_angle.Data[df_temp.Data <= min_t] = 1
-        df = df_angle
-
-        # drop rest blocks < minimum_rest_block minutes (except first and last)
-        df["block"] = (df.Data.diff().ne(0)).cumsum()
-        groups, iter_count = df.groupby(by="block"), 0
-        for group in groups:
-            iter_count += 1
-            if iter_count == 1 or iter_count == len(groups):
-                continue
-            if (
-                group[1]["Data"].sum() == 0
-                and len(group[1]) < 12 * minimum_rest_block
-            ):
-                df.Data[group[1].index[0]: group[1].index[-1]] = 1
-
-        # drop active blocks < allowed_rest_break minutes (except first and last)
-        df["block"] = (df.Data.diff().ne(0)).cumsum()
-        groups, iter_count = df.groupby(by="block"), 0
-        for group in groups:
-            iter_count += 1
-            if iter_count == 1 or iter_count == len(groups):
-                continue
-            if (
-                len(group[1]) == group[1]["Data"].sum()
-                and len(group[1]) < 12 * allowed_rest_break
-            ):
-                df.Data[group[1].index[0]: group[1].index[-1]] = 0
-
-        # get longest block
-        df["block"] = (df.Data.diff().ne(0)).cumsum()
-        best = 0
-        mrp = []
-        for group in df.groupby(by="block"):
-            if group[1]["Data"].sum() == 0 and len(group[1]) > best:
-                best = len(group[1])
-                mrp = [group[1].index[0], group[1].index[-1] + pd.Timedelta("5m")]
-
-        # save predictions
-        df.drop(columns=["block"], inplace=True)
-        df.to_hdf(
-            sub_dst
-            + "/major_rest_period/rest_periods_day_{}.h5".format(
-                str(count).zfill(2)
-            ),
-            key="rest_period_data_24hr",
-            mode="w",
-        )
-
-        mrps.append([count, mrp, available_hours])
-
-    # aggregate and save the major rest period for each day
-    mrps = pd.DataFrame(mrps)
-    mrps.columns = header
-    mrps.set_index("day", inplace=True)
-    results_directory = "/major_rest_period/{}_major_rest_periods.csv".format(src_name)
-    mrps.to_csv(sub_dst + results_directory)
-
-
 def sleep_wake_predict():
-    """Run sleep wake prediction based on the activity index feature."""
+    """
+    Runs sleep wake prediction based on the activity index feature.
+    Returns value "1" (awake) or "0" (asleep) for each timestamp.
+    Saves prediction as CSV file in results directory.
+    """
     os.mkdir(sub_dst + "/sleep_wake_predictions")
     count = 0
     # get days
@@ -468,114 +331,10 @@ def sleep_wake_predict():
         df.to_csv(sub_dst + "/result_sleep_prediction.csv")
 
 
-'''def calculate_endpoints():
-    """Calculate the metrics and endpoints of interest."""
-    os.mkdir(sub_dst + "/sleep_endpoints")  # set up output directory
-    count = 0
-    # get days
-    days = sorted(
-        [
-            sub_dst + "/sleep_wake_predictions/" + i
-            for i in os.listdir(sub_dst + "/sleep_wake_predictions/")
-            if ".DS_Store" not in i
-        ]
-    )
-
-    # get major rest periods for each day
-    mrps = pd.read_csv(
-        sub_dst
-        + "/major_rest_period/{}_major_rest_periods.csv".format(src_name),
-        parse_dates=True,
-        index_col="day",
-    )
-    endpoints = []
-    for day in days:
-        count += 1
-        df = pd.read_hdf(day)
-        # get and format times
-        times = mrps.loc[count].major_rest_period
-        try:
-            idt = times.index("[T")
-            times = times[: idt + 1] + "pd." + times[idt + 1:]
-            idt = times.index(", ")
-            times = times[: idt + 2] + "pd." + times[idt + 2:]
-            times = eval(times)
-            df = df.loc[times[0]: times[1]]
-        except ValueError:
-            pass
-
-        # get total sleep time
-        tst = len(df) - sum(df.values)
-
-        # get percent time asleep
-        pct_time_sleep = 100.0 * (len(df) - sum(df.values)) / float(len(df))
-
-        # get wake after sleep onset
-        waso = df.loc[df.idxmin()[0]:]
-        waso = waso.sum()[0]
-
-        # get sleep onset latency
-        sleep_onset_lat = (df.idxmin()[0] - df.index[0]).total_seconds() / 60.0
-
-        # number of wake bouts
-        num_wake_bouts = 0
-        wake_bout_df = df.copy()
-        wake_bout_df["block"] = (
-            wake_bout_df.sleep_predictions.diff().ne(0)
-        ).cumsum()
-        for group in wake_bout_df.groupby(by="block"):
-            if group[1]["sleep_predictions"].sum() > 0:
-                num_wake_bouts += 1
-        endpoints.append(
-            [
-                int(count),
-                int(tst[0]),
-                int(np.round(pct_time_sleep[0])),
-                int(waso),
-                int(sleep_onset_lat),
-                int(num_wake_bouts),
-            ]
-        )
-
-    # build and save output dataframe
-    hdr = [
-        "day",
-        "total_sleep_time",
-        "percent_time_asleep",
-        "waso",
-        "sleep_onset_latency",
-        "number_wake_bouts",
-    ]
-    endpoints = pd.DataFrame(endpoints)
-    endpoints.columns = hdr
-    endpoints.set_index(endpoints.day, inplace=True)
-    endpoints.drop(columns="day", inplace=True)
-    endpoints.to_csv(sub_dst + "/sleep_endpoints/sleep_endpoints_summary.csv")'''
-
-
-'''def aggregate_results():
-    """Aggregates all results in a single folder."""
-    os.mkdir(sub_dst + "/results")  # set up output directory
-    # collect results files
-    srcs = []
-    srcs += [
-        sub_dst + "/major_rest_period/" + x
-        for x in os.listdir(sub_dst + "/major_rest_period")
-        if ".csv" in x and ".DS_Store" not in x
-    ]
-    srcs += [
-        sub_dst + "/sleep_endpoints/" + x
-        for x in os.listdir(sub_dst + "/sleep_endpoints")
-        if ".DS_Store" not in x
-    ]
-
-    # aggregate
-    for src in srcs:
-        copy(src, sub_dst + "/results")'''
-
-
 def clear_data():
-    """Clears all intermediate data, keeping only results."""
+    """
+    Clears all intermediate data and directories, keeping only results.
+    """
     # collect directories for deletion
     direcs = [
         os.path.join(sub_dst, x)
@@ -593,27 +352,17 @@ def clear_data():
 def run_sleep_detection():
     print('Sleep Detection gestartet')
     try:
-        rmtree(sub_dst)
-        print('result Ordner geleert')
+        rmtree(sub_dst)  # removes old files from result directory.
+        print('emptied result directory')
     except OSError:
-        print('result Ordner war schon leer')
-    print('mach output dir')
+        print('result was already empty')
     os.mkdir(sub_dst)  # set up output directory
-    # split the data into 24 hour periods
-    print("Loading CSV data...")
-    split_days_geneactiv_csv()
-    print("Extracting activity index...")
+    print("Loading CSV data, split data into 24 hour periods...")
+    split_csv_acc_data_in_days()
+    print("Extracting activity index from accelerometer data...")
     extract_activity_index()
-    # print("Detecting major rest period...")
-    # major_rest_period()
     print("Running sleep/wake predictions...")
     sleep_wake_predict()
-    # print("Calculating endpoints...")
-    # calculate_endpoints()
-    # print("Aggregating results...")
-    # aggregate_results()
     print("Clearing intermediate data...")
     clear_data()
-    result = os.open(sub_dst + "/result_sleep_prediction.csv", os.O_RDONLY)
-    print(str(result))
-    return str(result)
+    return

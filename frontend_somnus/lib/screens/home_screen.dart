94ca3a798db9c4f4c922198273b444647857b549
Stephanie Scheibe <Stephanie.Scheibe@Student.HTW-Berlin.de>
2021-01-23 04:40:33 +0100

    Basic structure HomeScreen

--- frontend_somnus/lib/screens/home_screen.dart
+++ frontend_somnus/lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frontend_somnus/screens/database_helper.dart';
 import 'package:loading_overlay/loading_overlay.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -26,16 +27,195 @@ class _HomeScreenState extends State<HomeScreen> {
                   fit: BoxFit.cover)),
           child: Scaffold(
             backgroundColor: Colors.transparent,
-            body: Center(
-                /*child: RaisedButton(
-                onPressed: bleDeviceController.reset,
-                child: Text("Reset BLE Connection"),
-              ),
-              child: RaisedButton(
-                onPressed: _writeData,
-                child: Text("write data to file"),
-              ),*/
+            body: StaggeredGridView.count(
+              physics: NeverScrollableScrollPhysics(), //disable scrolling
+              crossAxisCount: 2,
+              crossAxisSpacing: 12.0,
+              mainAxisSpacing: 12.0,
+              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 40.0),
+              children: <Widget>[
+                _buildTile(
+                  Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text('Letzte Aufzeichnung',
+                                  style: TextStyle(color: Colors.blueAccent)),
+                              Text('23.01.2021',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 34.0))
+                            ],
+                          ),
+                          Material(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(24.0),
+                              child: Center(
+                                  child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Icon(Icons.timeline,
+                                    color: Colors.white, size: 30.0),
+                              )))
+                        ]),
+                  ),
                 ),
+                _buildTile(
+                    Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Material(
+                                color: Colors.teal,
+                                shape: CircleBorder(),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(16.0),
+                                  child: Icon(Icons.settings_applications,
+                                      color: Colors.white, size: 30.0),
+                                )),
+                            Padding(padding: EdgeInsets.only(bottom: 16.0)),
+                            Text('General',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 24.0)),
+                            Text('Images, Videos',
+                                style: TextStyle(color: Colors.black45)),
+                          ]),
+                    ),
+                    color: Color(0xFFa117f2)),
+                _buildTile(
+                    Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Material(
+                                color: Colors.amber,
+                                shape: CircleBorder(),
+                                child: Padding(
+                                  padding: EdgeInsets.all(16.0),
+                                  child: Icon(Icons.notifications,
+                                      color: Colors.white, size: 30.0),
+                                )),
+                            Padding(padding: EdgeInsets.only(bottom: 16.0)),
+                            Text('Alerts',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 24.0)),
+                            Text('All ',
+                                style: TextStyle(color: Colors.black45)),
+                          ]),
+                    ),
+                    color: Color(0xFFfe019a)),
+                // _buildTile(
+                //   Padding(
+                //       padding: const EdgeInsets.all(24.0),
+                //       child: Column(
+                //         mainAxisAlignment: MainAxisAlignment.start,
+                //         crossAxisAlignment: CrossAxisAlignment.start,
+                //         children: <Widget>[
+                //           Row(
+                //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //             crossAxisAlignment: CrossAxisAlignment.start,
+                //             children: <Widget>[
+                //               Column(
+                //                 mainAxisAlignment: MainAxisAlignment.start,
+                //                 crossAxisAlignment: CrossAxisAlignment.start,
+                //                 children: <Widget>[
+                //                   Text('Revenue',
+                //                       style: TextStyle(color: Colors.green)),
+                //                   Text('\$16K',
+                //                       style: TextStyle(
+                //                           color: Colors.black,
+                //                           fontWeight: FontWeight.w700,
+                //                           fontSize: 34.0)),
+                //                 ],
+                //               ),
+                //             ],
+                //           ),
+                //           Padding(padding: EdgeInsets.only(bottom: 4.0)),
+                //         ],
+                //       )),
+                // ),
+                _buildTile(
+                  Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text('Shop Items',
+                                  style: TextStyle(color: Colors.redAccent)),
+                              Text('173',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 34.0))
+                            ],
+                          ),
+                          Material(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(24.0),
+                            child: Center(
+                              child: Padding(
+                                padding: EdgeInsets.all(16.0),
+                                child: Icon(Icons.store,
+                                    color: Colors.white, size: 30.0),
+                              ),
+                            ),
+                          )
+                        ]),
+                  ),
+                  // onTap: () => Navigator.of(context)
+                  //     .push(MaterialPageRoute(builder: (_) => null)),
+                  color: Color(0xFF20d2f4),
+                ),
+                _buildTile(
+                  Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          FlatButton(
+                            onPressed: null,
+                            child: Text('Connect to Device'),
+                          ),
+                          FlatButton(
+                            onPressed: null,
+                            child: Text('Disconnect'),
+                          ),
+                        ]),
+                  ),
+                  // onTap: () => Navigator.of(context)
+                  //     .push(MaterialPageRoute(builder: (_) => null)),
+                  color: Colors.transparent,
+                ),
+              ],
+              staggeredTiles: [
+                StaggeredTile.extent(2, 110.0),
+                StaggeredTile.extent(1, 180.0),
+                StaggeredTile.extent(1, 180.0),
+                //StaggeredTile.extent(2, 220.0),
+                StaggeredTile.extent(2, 110.0),
+                StaggeredTile.extent(2, 110.0), //Buttons
+              ],
+            ),
           ),
         ),
         isLoading: _isLoading,
@@ -43,5 +223,21 @@ class _HomeScreenState extends State<HomeScreen> {
         progressIndicator: CircularProgressIndicator(),
       ),
     );
+  }
+
+  Widget _buildTile(Widget child, {Function() onTap, Color color}) {
+    return Material(
+        color: color,
+        elevation: 14.0,
+        borderRadius: BorderRadius.circular(5.0), //original: 12
+        shadowColor: Color(0x802196F3),
+        child: InkWell(
+            // Do onTap() if it isn't null, otherwise do print()
+            onTap: onTap != null
+                ? () => onTap()
+                : () {
+                    print('Not set yet');
+                  },
+            child: child));
   }
 }
